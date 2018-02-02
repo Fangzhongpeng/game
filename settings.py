2d636383648bed5ff18ee79044696be836cbadc9
Fangzp_ID <Fangzp_ID@163.com>
2018-02-02 15:20:33 +0800

    Initial commit

---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings():
+        def  __init__(self):
+                self.screen_width = 600
+                self.screen_height = 400
+                self.bg_color = (0,0,0)
+                self.ship_speed_factor =1
+                self.bullet_speed_factor = 1
+                self.bullet_width = 3
+                self.bullet_height = 15
+                self.bullet_color = 60,60,60
+                self.bullets_allowed  = 50

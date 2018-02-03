92a97894f5928c2412c6dca7c6efdb578efd56e4
Fangzp_ID <Fangzp_ID@163.com>
2018-02-03 16:03:50 +0800

    2018/2/3

--- settings.py
+++ settings.py
@@ -5,6 +5,9 @@ class Settings():
                 self.bg_color = (0,0,0)
                 self.ship_speed_factor =1
                 self.bullet_speed_factor = 1
+                self.alien_speed_factor  = 1
+                self.fleet_drop_speed = 2
+                self.fleet_direction = 1
                 self.bullet_width = 3
                 self.bullet_height = 15
                 self.bullet_color = 60,60,60

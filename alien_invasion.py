c1514277d65b42de32542e4650cf7aa6627dcae8
Fangzp_ID <Fangzp_ID@163.com>
2018-02-07 15:51:10 +0800

    2018/2/7   without scoring system

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from game_stats import GameStats
+from button import Button
 from ship import Ship
 import  game_functions as gf
 def run_game():
@@ -12,6 +13,7 @@ def run_game():
        screen = pygame.display.set_mode(
                      (ai_settings.screen_width ,ai_settings.screen_height))
        pygame.display.set_caption("you need python")
+       play_button = Button(ai_settings,screen,"Play")
        stats = GameStats(ai_settings)
        ship = Ship(ai_settings,screen)
        #创建一个用于存储子弹的编组
@@ -21,11 +23,11 @@ def run_game():
        gf.create_fleet(ai_settings,screen,ship,aliens)
        #开始游戏主循环
        while True:
-               gf.check_events(ai_settings,screen,ship,bullets)
+               gf.check_events(ai_settings,screen,stats,play_button,ship,aliens,bullets)
                if stats.game_active:
                        ship.update()
                        gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
                        gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
-                       gf.update_screen(ai_settings,screen,ship,aliens,bullets)
+               gf.update_screen(ai_settings,screen,stats,ship,aliens,bullets,play_button)
 run_game()
 
--- settings.py
+++ settings.py
@@ -3,13 +3,22 @@ class Settings():
                 self.screen_width = 600
                 self.screen_height = 400
                 self.bg_color = (0,0,0)
-                self.ship_speed_factor =1
                 self.ship_limit = 3
-                self.bullet_speed_factor = 1
-                self.alien_speed_factor  = 1
-                self.fleet_drop_speed = 2
-                self.fleet_direction = 1
                 self.bullet_width = 3
                 self.bullet_height = 15
                 self.bullet_color = 60,60,60
                 self.bullets_allowed  = 50
+                self.fleet_drop_speed = 2
+                self.speedup_scale = 1.1
+                self.initialize_dynamic_settings()
+        def initialize_dynamic_settings(self):
+                        self.ship_speed_factor = 1
+                        self.bullet_speed_factor = 1
+                        self.alien_speed_factor  = 1
+                        self.fleet_direction = 1
+        def increase_speed(self):
+                        self.ship_speed_factor *=self.speedup_scale
+                        self.bullet_speed_factor *= self.speedup_scale
+                        self.alien_speed_factor *= self.speedup_scale
+
+
